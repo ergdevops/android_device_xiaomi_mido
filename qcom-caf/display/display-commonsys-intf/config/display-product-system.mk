ac6fd9e19ee45c2cfb12f2128ec4831a53708aac
ergdevops <honor6firmware@gmail.com>
2023-03-25 06:40:00 +0000

    Revert "mido: Update Display HAL from LA.UM.10.6.2.r1-01900-89xx.0"
    This reverts commit f08c6ee05ba4f2be9db1d7befc01e74da4ca7693.

--- qcom-caf/display/display-commonsys-intf/config/display-product-system.mk
+++ qcom-caf/display/display-commonsys-intf/config/display-product-system.mk
@@ -1,18 +1,26 @@
-PRODUCT_PACKAGES += vendor.display.config@1.0 \
+PRODUCT_PACKAGES += libdisplayconfig \
+                    libqdMetaData \
+                    libqdMetaData.system \
+                    libgralloc.qti \
+                    libdrm \
+                    vendor.display.config@1.0 \
                     vendor.display.config@1.1 \
                     vendor.display.config@1.2 \
                     vendor.display.config@1.3 \
                     vendor.display.config@1.4 \
                     vendor.display.config@1.5 \
-                    vendor.qti.hardware.display.config-V1-ndk_platform \
-                    vendor.qti.hardware.display.config-V2-ndk_platform \
-                    vendor.qti.hardware.display.config-V3-ndk_platform \
-                    vendor.qti.hardware.display.config-V4-ndk_platform \
-                    vendor.qti.hardware.display.config-V5-ndk_platform \
-                    vendor.qti.hardware.display.config-V6-ndk_platform
+                    libsmomo.qti \
+                    liblayerext.qti \
+                    libsmomoconfig.qti \
+                    libcomposerextn.qti \
+                    libdisplayconfig.qti
 
-SOONG_CONFIG_NAMESPACES += qtiunifeddraw
+SOONG_CONFIG_NAMESPACES += qtidisplaycommonsys
 # Soong Keys
-SOONG_CONFIG_qtiunifeddraw := qtiunifeddraw_enabled
+SOONG_CONFIG_qtidisplaycommonsys := displayconfig_enabled
 # Soong Values
-SOONG_CONFIG_qtiunifeddraw_qtiunifeddraw_enabled := true
+SOONG_CONFIG_qtidisplaycommonsys_displayconfig_enabled := false
+
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+    SOONG_CONFIG_qtidisplaycommonsys_displayconfig_enabled := true
+endif
